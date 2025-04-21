9aec25279227d9a2b91962eb2bede7e084424b8a
jk1018 <1018jkjk@gmail.com>
2025-04-21 19:47:13 +0900

    fix : 네임스페이스 구성입니다

--- Revolver_6/GameManager.cs
+++ Revolver_6/GameManager.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Revolver_6
+﻿namespace Revolver_6
 {
     internal class GameManager
     {
+    
     }
 }
