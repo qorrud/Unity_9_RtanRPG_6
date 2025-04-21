d232aafff3d86db46017697d625eecb090dbd307
GroupPaper <thdwlsdnww@naver.com>
2025-04-21 20:25:39 +0900

    feat_타이틀 기능 추가
    기본 Home 설정과 각 보기로 넘어갈 수 있는 상태, 종료 버튼 추가

---
+++ Revolver_6/Title.cs
@@ -0,0 +1,50 @@
+﻿internal class Title
+{
+    public static void GameStart()
+    {
+        Home();
+    }
+
+    static void Home()
+    {
+        Console.Clear();
+        Console.WriteLine("스파르타 던전에 오신 여러분 환영합니다.");
+        Console.WriteLine("이제 전투를 시작할 수 있습니다");
+
+        Console.WriteLine("1. 상태 보기");
+        Console.WriteLine("2. 전투 시작");
+        Console.WriteLine("0. 게임 종료");
+
+        while (true)
+        {
+            Console.WriteLine("원하시는 행동을 입력해주세요");
+            Console.Write(">>  ");
+
+            string input = Console.ReadLine() ?? "";
+
+            switch (input)
+            {
+                case "1":
+                    Console.Clear();
+                    //상태창
+                    break;
+
+                case "2":
+                    Console.Clear();
+                    //전투 시작
+                    break;
+
+                case "0":
+                    Console.WriteLine("게임을 종료합니다.");
+                    Thread.Sleep(1000);
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.Clear();
+                    Console.WriteLine("잘못된 입력입니다.");
+                    break;
+            }
+        }
+    }
+}
