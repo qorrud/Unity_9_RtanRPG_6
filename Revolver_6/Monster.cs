c3f4862e77ea1233f1ebdbf948a81d246fa3ec95
qorrud <son980503@gmail.com>
2025-04-22 14:53:51 +0900

    Update Monster.cs

--- Revolver_6/Monster.cs
+++ Revolver_6/Monster.cs
@@ -11,17 +11,21 @@ namespace Revolver_6
             public int Gold { get; set; }
             public int Exp { get; set; }
 
+            public int Index { get; set; }
+
 
             public MonsterStat()
             {
                 Name = "오류";
-                Level = -1;
+                Level = 0;
                 Attack = 0;
-                HP = 1;
-                Exp = 1;
+                HP = 0;
+                Gold = 0;
+                Exp = 0;
+                Index = 0;
 
             }
-            public MonsterStat(string name, int level, int hp, int attack, int gold, int exp) //MonsterStat 객체를 만들 때 필요한 생성자
+            public MonsterStat(string name, int level, int hp, int attack, int gold, int exp, int index) //MonsterStat 객체를 만들 때 필요한 생성자
             {                                                                                  //그 객체를 만들 때 안에 들어갈 값을 한 번에 설정하기 위함
                 Name = name;                                                                    // 이 생성자가 없다면 32번째줄에 코드가 하나씩 입력해야해서 길어짐
                 Level = level;
@@ -29,6 +33,7 @@ namespace Revolver_6
                 HP = hp;
                 Gold = gold;
                 Exp = exp;
+                Index = index;
             }
 
             public void Display()
@@ -37,19 +42,40 @@ namespace Revolver_6
             }
         }
 
-            public static List<MonsterData.MonsterStat> MonsterList = new List<MonsterData.MonsterStat>
-        {                  //name,      lv, hp, atk, gold, exp
-            new MonsterData.MonsterStat( "미니언"   , 2 , 6 , 15 , 0 ,0),
-            new MonsterData.MonsterStat("대포 미니언", 5, 15, 25, 0, 0),
-            new MonsterData.MonsterStat("공허충"    , 3, 10, 10, 0, 0),
-            new MonsterData.MonsterStat("공허의 전령", 8, 20, 20, 0, 0 )
+        public static List<MonsterData.MonsterStat> MonsterList = new List<MonsterData.MonsterStat>
+        {                  //           name,      lv, hp, atk, gold, exp, index
+            new MonsterData.MonsterStat( "미니언"   , 2, 6, 15, 0, 0, 1),
+            new MonsterData.MonsterStat("대포 미니언", 5, 15, 25, 0, 0, 2),
+            new MonsterData.MonsterStat("공허충"    , 3, 10, 10, 0, 0, 3),
+            new MonsterData.MonsterStat("공허의 전령", 8, 20, 20, 0, 0, 4),
         };
-            public class MonsterFactory
+        public class MonsterFactory
+        {
+            Random random = new Random();
+            int[] MonsterNumber = new int[4]; // 일단 몬스터 수는 4자리로 고정
+
+            public void RandomSpawn()
             {
-
-
+                for (int i = 0; i < 3; i++)
+                {
+                    MonsterNumber[i] = random.Next(1, 4);
+                }
             }
+            List<MonsterStat> spawnMonster = new List<MonsterStat>();
+            public void MonsterSpwan()
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    int index = MonsterNumber[i];
+                    MonsterStat SpawnedMonster = MonsterList.Find(m => m.Index == index);
+                    MonsterStat monster = new MonsterStat(SpawnedMonster);
+                    spawnMonster.Add(monster);
+                }
 
-        
+
+        }
+        }
+
+
     }
 }
