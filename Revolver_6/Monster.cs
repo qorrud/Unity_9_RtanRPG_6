f16aca054cc5be17ac566d5d7ca215224c5932bc
qorrud <son980503@gmail.com>
2025-04-22 11:52:42 +0900

    Update Monster.cs

--- Revolver_6/Monster.cs
+++ Revolver_6/Monster.cs
@@ -37,12 +37,9 @@ namespace Revolver_6
             }
         }
 
-        
-
-
             public static List<MonsterData.MonsterStat> MonsterList = new List<MonsterData.MonsterStat>
         {                  //name,      lv, hp, atk, gold, exp
-            new MonsterData.MonsterStat( "미니언"   , 2 , 6 , 15 , 0 , 0),
+            new MonsterData.MonsterStat( "미니언"   , 2 , 6 , 15 , 0 ,0),
             new MonsterData.MonsterStat("대포 미니언", 5, 15, 25, 0, 0),
             new MonsterData.MonsterStat("공허충"    , 3, 10, 10, 0, 0),
             new MonsterData.MonsterStat("공허의 전령", 8, 20, 20, 0, 0 )
