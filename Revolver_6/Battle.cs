3a28bf2a1ef1378890978f467d3718330bceef63
hbks0224 <hbks1234@gmail.com>
2025-04-22 14:20:47 +0900

    플레이어 턴 추가

--- Revolver_6/Battle.cs
+++ Revolver_6/Battle.cs
@@ -21,14 +21,17 @@ namespace Revolver_6
         //    - 몬스터가 죽었다면 체력 대신 Dead 으로 표시됩니다.
         //    - 몬스터가 죽었다면 해당 몬스터에 텍스트는 전부 어두운 색으로 표시합니다.
         static bool Player_turn = true; //플레이어의 턴은 true 몬스터의 턴은 false로 할게요
+
+
         //int rage = 0; // 쓸 지는 모르겠지만 광폭화 스택입니다. 플레이어, 몬스터의 턴이 끝나면 1씩 증가하고 일정 수치 이상이 된다면 몬스터의 스탯이 뻥튀기 되는 거 넣어볼 생각인데 나중에..
 
-        internal static void My_Phase() //플레이어의 턴일때 실행되는 함수입니다. 플레이어의 턴이 끝나면  turn 은 false로 변경해서 적의 턴으로 넘어갑니다.
+        internal static void My_Phase()
+        //플레이어의 턴일때 실행되는 함수입니다. 플레이어의 턴이 끝나면  turn 은 false로 변경해서 적의 턴으로 넘어갑니다.
         {
-            //Spawn_Random_Monster() 랜덤 몬스터 소환 로직 넣을껍니다.
+            //MonsterFactory() 랜덤 몬스터 소환 로직 넣을껍니다.
 
 
-            Typing("Red","Battle!");
+            Typing("Red", "Battle!");
             foreach (MonsterData.MonsterStat monster in MonsterData.MonsterList) // 몬스터 정보입니다.
             {
                 monster.Display();
@@ -59,24 +62,90 @@ namespace Revolver_6
 
             }
 
-            Typing("Black", $"대상을 선택해주세요. (1~{monster_index}\n>>");
+            Typing("Black", $"대상을 선택해주세요. (1~{monster_index - 1}\n>>"); //n번째 몬스터 인덱스 출력하면 몬스터 인덱스값이 n+1되서 -1함
 
-            int input = WhatNum(1, monster_index);
+            int input = WhatNum(1, monster_index - 1);
             Console.Clear();
-            
-            if ( input == monster_index)
+
+            //몬스터 생성 로직에 따라 코드 넣을 생각입니다. 
+            Typing("Red", "Battle!\n");
+            //플레이어의 이름 의 공격!
+            TypingWrite("white", $"Lv");
+            TypingWrite("red", $"{monster.Level}");
+            TypingWrite("white", $"{monster.Name}을(를) 맞췄습니다. [데미지 : "); //TypingWrite 함수 갱신되기 전에 사용
+            int Player_baseAttack = Player.basePower;
+
+            int error = (int)Math.Ceiling(Player_baseAttack * 0.1); //공격력의 오차 설정
+            int min = Player_baseAttack - error; //최소 데미지
+            int max = Player_baseAttack + error; //최대 데미지
+            Random random = new Random();
+            int player_damage = random.Next(min, max + 1); // 오차에 따른 랜덤 데미지 설정하기
+            TypingWrite("red", $"{player_damage}\n");
+            TypingWrite("white", $"Lv");
+            TypingWrite("red", $"{monster.Level}");
+            TypingWrite("white", $"{monster.Name}\n");
+            TypingWrite("white", "HP");
+            TypingWrite("red", $"{monster.HP}");
+            TypingWrite("white", "->");
+            monster.HP -= player_damage; //데미지 계산
+            if (monster.HP < 0) //체력이 마이너스가 되면 안되니까 음수로 되면 0으로 설정합니다.
             {
-                Typing("Red", "Battle!\n");
-                //플레이어의 이름 의 공격!
-                TypingWrite("white", $"Lv{monster.Level}");
-                TypingWrite("white",$"{monster.Name}을(를) 맞췄습니다. [데미지 : "); //TypingWrite 함수 갱신되기 전에 사용
-                Random random_Attack = new Random(); //공격력은 10%의 오차
-                TypingWrite("\n")
+
+                monster.HP = 0;
+            }
+            if (monster.HP == 0) // 몬스터의 체력이 0이 된다면 죽이는 로직
+            {
+                TypingWrite("gray", " Dead\n");
+            }
+            else //죽지 않았다면 남은 체력을 표시
+            {
+                TypingWrite("white", $" {monster.HP - player_damage}\n");
+
+
+
+
+
+
+                Typing("red", "0.", "white", " 다음\n\n>>");
+
+                WhatNum(0, 0);
+
+                Player_turn = false; //플레이어의 턴이 종료됐으므로 false로 변경 후 몬스터의 턴 시작
+
+                Console.Clear();
+                if (Player_turn == false) //아 근데 저거 bool값 굳이 필요한가 싶기도 하고..
+                {
+                    Monster_Pahse(); //몬스터 페이즈 실행
+
+
+                }
+
 
 
             }
+        }
+
+        internal static void Monster_Pahse() //몬스터 턴입니다.
+        {
+            Typing("Red", "Battle!\n");
+            foreach (MonsterData.MonsterStat monster in MonsterData.MonsterList) // 몬스터 정보입니다.
+            {
+                if (monster.HP == 0)
+                {
+                    Typing("white", "Lv.", "red", $"{monster.Level}", "white", "은 사망했기 때문에 공격 할 수 없다!");
+                    Typing("white", "Lv.", "red", $"{monster.Level}", "white", $"{monster.Name}은(는) 사망했기 때문에 공격 할 수 없다!");
 
 
+                }
+                else {
+                    Typing("white", "Lv.", "red", $"{monster.Level}", "white", $"{monster.Name}의 공격!");
+                    Typing("white", $"{Player.name}을(를) 맞췃습니다.");
+
+
+
+                }
+
+            }
 
 
         }
