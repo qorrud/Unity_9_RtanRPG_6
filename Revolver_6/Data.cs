8ac767557dbec0f1122e097864e1e5db53817fe3
qorrud <son980503@gmail.com>
2025-04-22 11:36:35 +0900

    Update Data.cs

--- Revolver_6/Data.cs
+++ Revolver_6/Data.cs
@@ -9,13 +9,8 @@ namespace Revolver_6
     internal class Data
     {
         public static PlayerStats Player = new PlayerStats();
-         public static List<MonsterData.MonsterStat> MonsterList = new List<MonsterData.MonsterStat>
-        {                  //name,      lv, hp, atk, gold, exp
-            new MonsterData.MonsterStat( "미니언"   , 2 , 6 , 15 , 0 , 0),
-            new MonsterData.MonsterStat("대포 미니언", 5, 15, 25, 0, 0),
-            new MonsterData.MonsterStat("공허충"    , 3, 10, 10, 0, 0),
-            new MonsterData.MonsterStat("공허의 전령", 8, 20, 20, 0, 0 )
-        };
+        
+        public static MonsterData.MonsterStat = new MonsterStat();
         
     }
 }
