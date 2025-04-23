fe0090c7fba1b8cb123fe972565903483b28f8b4
qorrud <son980503@gmail.com>
2025-04-23 11:02:12 +0900

    여관, 몬스터 최대체력
    여관 가격 설정, 몬스터 최대체력 출력 질문

---
+++ Revolver_6/Inn.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Revolver_6.Helper;
+
+namespace Revolver_6
+{
+    internal class Inn
+    {
+    public void InnIntro()
+        {
+            // Helper.Typing("magenta", monster[i].Level, "", $" {monster[i].Name}  HP ", "magenta", "monster[i].HP / {monster[i].MaxHP}",0);
+            Helper.Typing("green", "당신은 포근한 여관에 들어섰습니다...");
+            Helper.Typing("green", "모닥불이 조용히 타오르고, 벽난로 위에는 사슴 박제가 걸려 있습니다.");
+
+            Thread.Sleep(1000);
+
+            Helper.Typing("cyan", "\"어서 오세요, 여행자님.\"");
+            Helper.Typing("cyan", "당신을 맞이하는 건 푸근한 인상의 여관 주인입니다.");
+            Helper.Typing("cyan", "\"긴 모험에 지치셨을 테니, 편히 쉬어가세요.\"");
+
+            Thread.Sleep(1000);
+
+            Helper.Typing("white", "\n■ 객실 종류 안내 ■"); // 가격 책정도 해야함
+            Helper.Typing("white", "1. 하급 객실");
+            Helper.Typing("white", "2. 중급 객실");
+            Helper.Typing("white", "3. 상급 객실");
+            Helper.Typing("white", "4. 최상급 객실");
+
+            Thread.Sleep(1000);
+
+            Helper.Typing("white", "\n[하급 객실] : 매우 협소하지만 최소한의 쉼을 제공합니다. 피로를","green", "아주 조금","white","풉니다.");
+            Helper.Typing("white", "[중급 객실] : 적당한 크기의 방. 따뜻한 담요와 식사가 제공됩니다. 피로를","green", "어느정도","white","풉니다.");
+            Helper.Typing("white", "[상급 객실] : 깔끔한 침구, 창가 자리. 피로가 빠르게 풀립니다. 피로를","green", "상당히","white","풉니다.");
+            Helper.Typing("white", "[최상급 객실] : 고급 침대와 아로마향 가득한 룸서비스. 피로를","green", "완벽하게","white","풉니다.");
+
+            Helper.Typing("gray", "\n※ 객실을 선택하려면 숫자를 입력하세요.");
+
+        }
+
+        public int InnSelect()
+        {
+            int input = Helper.WhatNum(1,4);
+
+            switch(input)
+            {
+                case 1: //하급 객실
+                    return 25; 
+                case 2: // 중급 객실
+                    //HealCount(50);
+                    return 50;
+                case 3: // 상급 객실
+                    //HealCount(75);
+                    return 75;
+                case 4: // 최상급 객실
+                    //HealCount(100);
+                    return 100;
+                default:
+                    return 0;
+            }
+        }
+
+    }
+    }
+
--- Revolver_6/Monster.cs
+++ Revolver_6/Monster.cs
@@ -7,6 +7,7 @@ namespace Revolver_6
             public string Name { get; set; }
             public int Level { get; set; }
             public int Attack { get; set; }
+            public int MaxHP { get; set; }
             public int HP { get; set; }
             public int Gold { get; set; }
             public int Exp { get; set; }
@@ -19,17 +20,19 @@ namespace Revolver_6
                 Name = "오류";
                 Level = 0;
                 Attack = 0;
+                MaxHP = 0;
                 HP = 0;
                 Gold = 0;
                 Exp = 0;
                 Index = 0;
 
             }
-            public MonsterStat(string name, int level, int hp, int attack, int gold, int exp, int index) //MonsterStat 객체를 만들 때 필요한 생성자
+            public MonsterStat(string name, int level,int max_hp, int hp, int attack, int gold, int exp, int index) //MonsterStat 객체를 만들 때 필요한 생성자
             {                                                                                  //그 객체를 만들 때 안에 들어갈 값을 한 번에 설정하기 위함
                 Name = name;                                                                    // 이 생성자가 없다면 32번째줄에 코드가 하나씩 입력해야해서 길어짐
                 Level = level;
                 Attack = attack;
+                MaxHP = max_hp;
                 HP = hp;
                 Gold = gold;
                 Exp = exp;
@@ -42,22 +45,27 @@ namespace Revolver_6
                 {
                     Helper.TypingWrite("blue", i+1,0);
                     Helper.TypingWrite("", " Lv.",0);
-                    Helper.Typing("magenta", monster[i].Level, "", $" {monster[i].Name}  HP ", "magenta", monster[i].HP,0);
+                    //Helper typing 미숙, 이따가 질문하기
+                    Helper.Typing("magenta", monster[i].Level, "", $" {monster[i].Name}  HP ", "magenta", "monster[i].HP / {monster[i].MaxHP}",0);
                     Thread.Sleep(200);
+                    // 몬스터의 체력이 0이 되면 Dead가 회색으로 출력되게끔
+                    if(monster[i].HP <= 0)
+                    {
+                        Helper.Typing("magenta", monster[i].Level, "", $" {monster[i].Name}  ", "gray", "Dead",0);
+                    }
                 }
             }
         }
 
         public static Dictionary<int, MonsterStat> MonsterList = new Dictionary<int, MonsterStat>()
-        {                  //           name,      lv, hp, atk, gold, exp, index
-            {1, new MonsterData.MonsterStat( "미니언"   , 2, 6, 15, 0, 0, 1)},
-            {2, new MonsterData.MonsterStat("대포 미니언", 5, 15, 25, 0, 0, 2)},
-            {3, new MonsterData.MonsterStat("공허충"    , 3, 10, 10, 0, 0, 3)},
-            {4, new MonsterData.MonsterStat("공허의 전령", 8, 20, 20, 0, 0, 4)},
+        {                  //           name,      lv, maxhp, hp, atk, gold, exp, index
+            {1, new MonsterData.MonsterStat( "미니언"   , 2, 6, 15, 15, 0, 0, 1)},
+            {2, new MonsterData.MonsterStat("대포 미니언", 5, 15, 25, 25, 0, 0, 2)},
+            {3, new MonsterData.MonsterStat("공허충"    , 3, 10, 10, 10, 0, 0, 3)},
+            {4, new MonsterData.MonsterStat("공허의 전령", 8, 20, 20, 20, 0, 0, 4)},
         };
         public class MonsterFactory
         {
-
 
             public static int[] RandomSpawn()
             {
@@ -79,6 +87,7 @@ namespace Revolver_6
                     Dummy[i].Name = MonsterList[a[i]].Name;
                     Dummy[i].Level = MonsterList[a[i]].Level;
                     Dummy[i].Attack = MonsterList[a[i]].Attack;
+                    Dummy[i].MaxHP = MonsterList[a[i]].MaxHP;
                     Dummy[i].HP = MonsterList[a[i]].HP;
                     Dummy[i].Gold = MonsterList[a[i]].Gold;
                     Dummy[i].Exp = MonsterList[a[i]].Exp;
